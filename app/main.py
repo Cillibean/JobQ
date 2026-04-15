f0a61b071b430129ae21145e70665cc32737cdcf
Cillibean <cillibean16@gmail.com>
2026-04-15 11:59:37 +0100

    added comment in order to push

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from app.api.routes import router
 from app.db.models import Base
 from app.db.session import engine
 
+# Create the FastAPI application
+
 app = FastAPI()
 
 @app.on_event("startup")
